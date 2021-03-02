a122e80bfa1f8320658cef2f6ec510effc2ea76a
Mannan-Goyal <mannang6@gmail.com>
2021-03-03 01:53:13 +0530

    Registration Feature Added

---
+++ models/user.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+//create user Schema & model
+const UserSchema = new Schema({
+    uname:{
+        type: String,
+        required: [true, 'Username is required'],
+        unique: true
+    },
+    name: {
+        type: String,
+        required: [true, 'Name is required']
+    },
+    email: {
+        type: String,
+        required: [true, 'Name is required'],
+        unique: true
+    },
+    pass:{
+        type: String,
+        required: [true, 'Password is required'],
+    },
+    regno: {
+        type: String,
+    }
+});
+
+const User = mongoose.model('user',UserSchema);
+module.exports = User;
---
+++ routers/api.js
@@ -0,0 +1,44 @@
+const express = require ('express');
+const router = express.Router();
+const User = require('../models/user');
+const bcrypt = require('bcrypt');
+
+//get list of users
+router.get('/user',function(req,res,next){
+    User.find({}).then(function(users){
+        res.send(users);
+    });
+});
+
+//show login page
+router.get('/user/login', (req, res) => {
+  res.render('login.hbs');
+});
+
+//show registration page
+router.get('/user/register', (req, res) => {
+    res.render('register.hbs');
+});
+
+//add new user to the db
+router.post('/user/register',function(req,res,next){
+    User.create(req.body).then((user)=>{
+        res.send(user);
+    });
+});
+
+//tried hashing didn't work out
+// router.post('/user/register', (req, res) => {
+//       const hashedPassword = bcrypt.hash(req.body.pass, 10).toString();
+//       User.create({
+//         name: req.body.name,
+//         email: req.body.email,
+//         uname: req.body.uname,
+//         regno: req.body.regno,
+//         pass: hashedPassword
+//       }).then((user)=>{
+//           res.send(user);
+//       });
+//   });
+
+module.exports = router;
