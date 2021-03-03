fffb128541b6b9e57bfd7fceeea0dec106dab6c9
Mannan-Goyal <mannang6@gmail.com>
2021-03-04 02:16:43 +0530

    Login/Registration without Hashing

--- models/user.js
+++ models/user.js
@@ -23,8 +23,45 @@ const UserSchema = new Schema({
     },
     regno: {
         type: String,
+    },
+    questions: {
+        type: [Number],
+        default: []
     }
-});
+    });
 
 const User = mongoose.model('user',UserSchema);
 module.exports = User;
+
+
+//OLD SCHEMA
+// const mongoose = require('mongoose');
+// const Schema = mongoose.Schema;
+
+// //create user Schema & model
+// const UserSchema = new Schema({
+//     uname:{
+//         type: String,
+//         required: [true, 'Username is required'],
+//         unique: true
+//     },
+//     name: {
+//         type: String,
+//         required: [true, 'Name is required']
+//     },
+//     email: {
+//         type: String,
+//         required: [true, 'Name is required'],
+//         unique: true
+//     },
+//     pass:{
+//         type: String,
+//         required: [true, 'Password is required'],
+//     },
+//     regno: {
+//         type: String,
+//     }
+// });
+
+// const User = mongoose.model('user',UserSchema);
+// module.exports = User;
--- routers/api.js
+++ routers/api.js
@@ -12,7 +12,32 @@ router.get('/user',function(req,res,next){
 
 //show login page
 router.get('/user/login', (req, res) => {
-  res.render('login.hbs');
+    res.render('login.hbs');
+});
+
+//Login
+router.post("/user/login", async (req,res)=>{
+    const email = req.body.email;
+    //const hashedPassword = await bcrypt.hash(req.body.pass,10);
+    const pass = req.body.pass;
+    User.findOne({email: email},(err,foundResults) => {
+        //console.log(req.body)
+        //console.log(foundResults.pass);
+        //console.log(hashedPassword);
+        //console.log(pass)
+        if(err){
+            console.log(err);
+        }
+        else{
+            //if(foundResults.pass == hashedPassword){
+            if(foundResults.pass == pass){
+                res.send("Login Successful!");
+            }
+            else{
+                res.send("Login Unsuccessful!");
+            }
+        }
+    });
 });
 
 //show registration page
@@ -20,25 +45,25 @@ router.get('/user/register', (req, res) => {
     res.render('register.hbs');
 });
 
-//add new user to the db
+//add new user to the db without hashing
 router.post('/user/register',function(req,res,next){
     User.create(req.body).then((user)=>{
         res.send(user);
     });
 });
 
-//tried hashing didn't work out
+//add new user to the db with hashing
 // router.post('/user/register', async (req, res) => {
-//       const hashedPassword = bcrypt.hash(req.body.pass, 10);
-//       User.create({
-//         name: req.body.name,
-//         email: req.body.email,
-//         uname: req.body.uname,
-//         regno: req.body.regno,
-//         pass: hashedPassword
-//       }).then((user)=>{
-//           res.send(user);
-//       });
-//   });
+//     const hashedPassword = await bcrypt.hash(req.body.pass,10);
+//         User.create({
+//             name: req.body.name,
+//             email: req.body.email,
+//             uname: req.body.uname,
+//             regno: req.body.regno,
+//             pass: hashedPassword
+//         }).then((user)=>{
+//         res.send(user);
+//     });
+//     });
 
 module.exports = router;
