c931408d6445e31041e43baaf8b071c205606ebb
theProgrammerDavid <davidvelho2awesome@gmail.com>
2021-03-03 23:19:56 +0530

    fix: hashing, mongoose stuff

--- routers/api.js
+++ routers/api.js
@@ -1,11 +1,17 @@
-const express = require ('express');
+const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
 const bcrypt = require('bcrypt');
 
+const hashedPassword = async function hashPass(passw) {
+    const salt = process.env.SALT;
+    const hash = await bcrypt.hash(passw, salt);
+    return hash
+}
+
 //get list of users
-router.get('/user',function(req,res,next){
-    User.find({}).then(function(users){
+router.get('/user', function (req, res, next) {
+    User.find({}).then(function (users) {
         res.send(users);
     });
 });
@@ -16,37 +22,41 @@ router.get('/user/login', (req, res) => {
 });
 
 //Login
-router.post("/user/login", async (req,res)=>{
-    const email = req.body.email;
-    let passw = req.body.pass;
-    const hashedPassword = async function hashPass(passw) {
-        let password = pass;
-        let saltRounds = 10;
-        //Note: Never define a constant salt in the source file
-        const salt = '$2b$10$GA94lFm2QPNNF80uz6UMFu';
-        const hash = await bcrypt.hash(password, salt);
-        return hash
+router.post("/user/login", async (req, res) => {
+    try {
+        const email = req.body.email;
+        let passw = req.body.pass;
+        const result = await User.findOne({ email: email, pass: await hashedPassword(passw) });
+        
+        if (result) {
+            res.json({ "login": true });
+        }
+        // if (result.pass === await hashedPassword(passw)) {
+        //     res.json({ "login": true });
+        // }
+        res.json({ "login": false });
     }
-    const pass = req.body.pass;
-    User.findOne({email: email},(err,foundResults) => {
-        //console.log(req.body);
-        //console.log(foundResults);
-        //console.log(foundResults.pass);
-        //console.log(hashedPassword);
-        //console.log(pass)
-        if(err){
-            console.log(err);
-        }
-        else{
-            if(foundResults.pass === hashedPassword(passw)){
-            //if(foundResults.pass == pass){
-                res.json({"login":true});
-            }
-            else{
-                res.json({"login":false});
-            }
-        }
-    });
+    catch (e) {
+        res.send();
+        // send some error code here
+    }
+    // const pass = req.body.pass;
+    // User.findOne({ email: email }, async (err, foundResults) => {
+    //     console.log(foundResults.pass)
+
+    //     if (err) {
+    //         console.log(err);
+    //     }
+    //     else {
+    //         if (foundResults.pass === await hashedPassword(passw)) {
+    //             //if(foundResults.pass == pass){
+    //             res.json({ "login": true });
+    //         }
+    //         else {
+    //             res.json({ "login": false });
+    //         }
+    //     }
+    // });
 });
 
 //show registration page
@@ -67,23 +77,16 @@ router.get('/user/register', (req, res) => {
 //add new user to the db with hashing
 router.post('/user/register', async (req, res) => {
     let passw = req.body.pass;
-    const hashedPassword = async function hashPass(passw) {
-        let password = pass;
-        let saltRounds = 10;
-        //Note: Never define a constant salt in the source file
-        const salt = '$2b$10$GA94lFm2QPNNF80uz6UMFu';
-        const hash = await bcrypt.hash(password, salt);
-        return hash
-    }
-        User.create({
-            name: req.body.name,
-            email: req.body.email,
-            uname: req.body.uname,
-            regno: req.body.regno,
-            pass: hashedPassword(passw)
-        }).then((user)=>{
+
+    User.create({
+        name: req.body.name,
+        email: req.body.email,
+        uname: req.body.uname,
+        regno: req.body.regno,
+        pass: await hashedPassword(passw)
+    }).then((user) => {
         res.send(user);
     });
-    });
+});
 
 module.exports = router;
